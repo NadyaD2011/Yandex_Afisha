499054d17b7fd03ec73cc5c19a608efbc295645a
NadyaEvp <t_evp@mail.ru>
2025-09-13 19:44:04 +0300

    Add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from places.models import Place
+
+@admin.register(Place)
 
 # Register your models here.
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title',)
--- places/models.py
+++ places/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
-print('ввв')
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=200, verbose_name='название')
+    description_short = models.TextField(verbose_name='краткое описание', blank=True)
+    description_long = models.TextField(verbose_name='полное описание')
+    lat = models.DecimalField(max_digits=50, decimal_places=20, verbose_name='широта')
+    lng = models.DecimalField(max_digits=50,  decimal_places=20, verbose_name='долгота')
