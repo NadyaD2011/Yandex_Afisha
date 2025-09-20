c5154d251370e266dbf7bdb3adae673c807f41fb
NadyaEvp <t_evp@mail.ru>
2025-09-20 19:03:13 +0300

    frontend takes a list of locations from the database

--- places/models.py
+++ places/models.py
@@ -10,8 +10,15 @@ class Place(models.Model):
     lat = models.DecimalField(max_digits=50, decimal_places=20, verbose_name='широта')
     lng = models.DecimalField(max_digits=50,  decimal_places=20, verbose_name='долгота')
 
+    def __str__(self) -> str:
+        return f'{self.title}'
+
 
 class Image(models.Model):
+    place = models.ForeignKey("Place", on_delete=models.CASCADE, related_name='imgs', verbose_name='место', null=True, blank=True)
     index = models.PositiveIntegerField(default=0, verbose_name='индекс картинки', db_index=True)
     title = models.CharField(max_length=200, verbose_name='название картинки')
     img = models.ImageField(verbose_name='картинка')
+
+    def __str__(self) -> str:
+        return f'{self.place}'
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -6,5 +6,6 @@ from where_to_go import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.open_map)
+    path('', views.open_map),
+    path("places/<int:place_id>/", views.parse_place_details, name="parse_place_details"),
 ]
