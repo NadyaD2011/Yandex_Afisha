fd9d0c5c0da024fe90a097ce52ef603acbfe8f3e
NadyaEvp <t_evp@mail.ru>
2025-09-20 18:16:06 +0300

    Move data from template to view

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Place, Image
 
+
 @admin.register(Place)
 @admin.register(Image)
 
-# Register your models here.
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,32 @@
 from django.http import HttpResponse
 from django.template import loader
+from places.models import Place
+
+from django.shortcuts import render
 
 
 def open_map(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    places = Place.objects.all()
+
+    features = [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [place.lat, place.lng]
+          },
+          "properties": {
+            "title": place.title,
+            "placeId": "moscow_legends",
+            "detailsUrl": "{% static '/places/moscow_legends.json' %}"
+          }
+        }
+        for place in places
+    ]
+    context = {'places': {
+            'type': "FeatureCollection",
+            'features': features
+        }
+    }
+
+    return render(request, 'index.html', context)
