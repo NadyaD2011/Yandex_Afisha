514a2f4b4b1b11c834230d7326cadc6b522df5f9
NadyaEvp <t_evp@mail.ru>
2025-09-20 19:23:23 +0300

    Data is obtained through links

--- places/views.py
+++ places/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.template import loader
+from places.models import Place, Image
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+
+def parse_place_details(request, place_id):
+    place = get_object_or_404(Place.objects.prefetch_related('imgs'), id=place_id)
+    images_urls = [image.img.url for image in place.imgs.all()]
+
+    payload = {
+        'title': place.title,
+        'imgs': images_urls,
+        'short_description': place.description_short,
+        'long_description': place.description_long,
+        'coordinates': {
+            'lng': float(place.lng),
+            'lat': float(place.lat),
+        }
+    }
+
+    return JsonResponse(payload, json_dumps_params={'ensure_ascii': False})
+
+
+def open_map(request):
+    places = Place.objects.all()
+
+    features = [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [place.lat, place.lng]
+          },
+          "properties": {
+            "title": place.title,
+            "placeId": "moscow_legends",
+            'description_short': place.description_short,
+            'description_long': place.description_long,
+            "coordinates": {
+                "lng": place.lng,
+                "lat": place.lat
+            },
+            'detailsUrl': reverse('parse_place_details', kwargs={'place_id': place.id})
+          }
+        }
+        for place in places
+    ]
+    context = {
+        'places': {
+            'type': "FeatureCollection",
+            'features': features
+        }
+    }
+
+    return render(request, 'index.html', context)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from where_to_go import views
+from places import views
 
 
 urlpatterns = [
+    path("places/<int:place_id>/", views.parse_place_details, name="parse_place_details"),
     path("admin/", admin.site.urls),
     path('', views.open_map),
-    path("places/<int:place_id>/", views.parse_place_details, name="parse_place_details"),
 ]
