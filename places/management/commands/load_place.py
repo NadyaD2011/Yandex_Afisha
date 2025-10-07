2f865e9b637b571c2eb3ae01cf12e7468fba38de
NadyaEvp <t_evp@mail.ru>
2025-10-07 18:12:50 +0300

    Create load_place.py

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,61 @@
+from decimal import Decimal
+from django.conf import settings
+from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+import requests
+
+import codecs
+import json
+import os
+
+from places.models import Place, Image
+
+
+def get_json_info_by_file(path):
+    with codecs.open(path, 'r', 'utf_8_sig') as file:
+        data = file.read()
+    parsed_place = json.loads(data)
+    return parsed_place
+
+
+def get_json_info_by_url(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def parse_place_with_images(url):
+    place = get_json_info_by_url(url)
+    parsed_place = Place.objects.get_or_create(
+        title=place['title'],
+        defaults={
+            'description_short': place['description_short'],
+            'description_long': place['description_long'],
+            'lat': Decimal(place['coordinates']['lat']),
+            'lng': Decimal(place['coordinates']['lng']),
+        }
+    )[0]
+    for img_number, img_url in enumerate(place['imgs']):
+        try:
+            response = requests.get(img_url)
+            response.raise_for_status()
+            img_content = ContentFile(response.content)
+            img_name = '{} {}.jpg'.format(img_number+1, parsed_place.title)
+            image_instance = Image(place=parsed_place)
+            image_instance.img.save(img_name, img_content, save=True)
+        except requests.exceptions.HTTPError or\
+                requests.exceptions.ConnectionError:
+            pass
+
+
+class Command(BaseCommand):
+    help = 'Download json data with url or with local path and parse it to DB.'
+
+    def handle(self, *args, **options):
+        parse_place_with_images(options['url'])
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'url', help='Path to json-file in the Inthernet'
+        )
